e2a151490bd3a9e30e1d48b77cf3d88b93ac9cc2
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-19 18:08:14 -0300

    Add connection method of typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 import * as express from 'express'
-import * as bdParser from 'body-parser'
 import routes from './routes'
 
 const app = express()
+
+createConnection()
 
 app.use(express.json())
 app.use(routes)
