a36a6a57a9ca92b08e3f4f9e2b9a987bb864fcb2
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-19 18:52:35 -0300

    Add new routes to get one task and create

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express'
+import { createTask, getOneTask, getTasks } from './controllers/TaskController'
 
 const routes = Router();
 
 routes.get('/', (req: Request, res: Response) => {
-    return res.json({message : 'Hello World'})
+    return res.json({message: "Hello everyone!"})
 })
+routes.get('/tasks', getTasks)
+routes.get('/task/:id', getOneTask)
+routes.post('/tasks', createTask)
 
 export default routes;
