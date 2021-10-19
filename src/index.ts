6578653de0e51a8d3f65fee9809171fe9c3a2502
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-18 23:22:09 -0300

    Started API with Express

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import 'reflect-metadata'
+import * as express from 'express'
+import * as bdParser from 'body-parser'
+import routes from './routes'
 
-createConnection().then(async connection => {
+const app = express()
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+app.use(express.json())
+app.use(routes)
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+app.listen(3333)
