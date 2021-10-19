f64134f379e0ca452c226f134f6a98aa858d5bfc
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-19 18:08:35 -0300

    Add new controller to Task entity

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { getRepository } from 'typeorm'
+import { Task } from '../entity/Task'
+
+module.exports = {
+    async index(req: Request, res: Response){
+        
+    },
+
+    async store(req: Request, res: Response){
+
+    },
+
+    async update(req: Request, res: Response){
+
+    },
+
+    async delete(req: Request, res: Response){
+
+    }
+}
