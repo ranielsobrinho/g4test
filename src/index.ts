3eb20030633fedc388382d60b8c82c1addb7674c
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-25 23:58:09 -0300

    Add port and a console.log showing url

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import * as express from 'express'
 import routes from './routes'
 
 const app = express()
+const port = process.env.PORT || 3333
 
 createConnection()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
