362afdf2dff7a6429d59b878484510ce6286d752
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-25 23:26:52 -0300

    Add new routes to finish, update and delete task

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express'
-import { createTask, getOneTask, getTasks } from './controllers/TaskController'
+import { createTask, deleteTask, finishTask, getOneTask, getTasks, updateTask } from './controllers/TaskController'
 
 const routes = Router();
 
@@ -9,5 +9,8 @@ routes.get('/', (req: Request, res: Response) => {
 routes.get('/tasks', getTasks)
 routes.get('/task/:id', getOneTask)
 routes.post('/tasks', createTask)
+routes.put('/task/:id', updateTask)
+routes.patch('/finish/:id', finishTask)
+routes.delete('/task/:id', deleteTask)
 
 export default routes;
