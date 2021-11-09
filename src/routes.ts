39090ffd9aa26d92af7285690afef1a603188cc8
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-09 17:05:53 -0300

    Add new routes to queue controller and queue validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from 'express'
+
 import { createUser, deleteUser, getOneUser, getUsers, updateUser } from './controllers/UserController'
+import { createQueue, deleteQueue, getQueues, updateQueue } from './controllers/QueueController'
 import validation from './middlewares/validationMiddleware'
 import userSchema from './validations/userValidation'
+import queueSchema from './validations/queueValidation'
 
 const routes = Router();
 
@@ -13,5 +16,10 @@ routes.get('/users/:id', getOneUser)
 routes.post('/users', validation(userSchema),createUser)
 routes.put('/users/:id', validation(userSchema), updateUser)
 routes.delete('/users/:id', deleteUser)
+
+routes.get('/queue', getQueues)
+routes.post('/queue', validation(queueSchema),createQueue)
+routes.put('/queue/:id', validation(queueSchema), updateQueue)
+routes.delete('/queue/:id', deleteQueue)
 
 export default routes;
