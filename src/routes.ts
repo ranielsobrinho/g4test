85f2ce425a3b5f7804a54cbb312ba43249c5beae
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-09 09:28:48 -0300

    Updated controller methods

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,15 @@
 import { Router, Request, Response } from 'express'
-import { createTask, deleteTask, finishTask, getOneTask, getTasks, updateTask } from './controllers/TaskController'
+import { createUser, deleteUser, getOneUser, getUsers, updateUser } from './controllers/UserController'
 
 const routes = Router();
 
 routes.get('/', (req: Request, res: Response) => {
-    return res.json({message: "Hello everyone!"})
+    return res.json({message: "Bem-vindo ao CRUD de usuário e fila.!"})
 })
-routes.get('/tasks', getTasks)
-routes.get('/task/:id', getOneTask)
-routes.post('/tasks', createTask)
-routes.put('/task/:id', updateTask)
-routes.patch('/finish/:id', finishTask)
-routes.delete('/task/:id', deleteTask)
+routes.get('/tasks', getUsers)
+routes.get('/task/:id', getOneUser)
+routes.post('/tasks', createUser)
+routes.put('/task/:id', updateUser)
+routes.delete('/task/:id', deleteUser)
 
 export default routes;
