abb8ad90b9f0311dde002146bf75562b71869afe
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-09 14:45:44 -0300

    Add validation with yup

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,17 @@
 import { Router, Request, Response } from 'express'
 import { createUser, deleteUser, getOneUser, getUsers, updateUser } from './controllers/UserController'
+import validation from './middlewares/validationMiddleware'
+import userSchema from './validations/userValidation'
 
 const routes = Router();
 
 routes.get('/', (req: Request, res: Response) => {
     return res.json({message: "Bem-vindo ao CRUD de usuário e fila.!"})
 })
-routes.get('/tasks', getUsers)
-routes.get('/task/:id', getOneUser)
-routes.post('/tasks', createUser)
-routes.put('/task/:id', updateUser)
-routes.delete('/task/:id', deleteUser)
+routes.get('/users', getUsers)
+routes.get('/users/:id', getOneUser)
+routes.post('/users', validation(userSchema),createUser)
+routes.put('/users/:id', validation(userSchema), updateUser)
+routes.delete('/users/:id', deleteUser)
 
 export default routes;
