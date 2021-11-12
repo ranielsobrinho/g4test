518201829b11073dac7ddc060871606b6f71f7c9
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-12 00:36:42 -0300

    Add authenticate to update and delete a user

--- src/routes.ts
+++ src/routes.ts
@@ -18,10 +18,10 @@ routes.get('/', (req: Request, res: Response) => {
 routes.get('/users', UserController.getUsers)
 routes.get('/users/:id', UserController.getOneUser)
 routes.post('/users', validation(userSchema), UserController.createUser)
-routes.put('/users/:id', validation(userSchema), UserController.updateUser)
-routes.delete('/users/:id', UserController.deleteUser)
+routes.put('/users/:id', authMiddleware, validation(userSchema), UserController.updateUser)
+routes.delete('/users/:id', authMiddleware, UserController.deleteUser)
 
-routes.get('/queue', authMiddleware, QueueController.getQueues)
+routes.get('/queue/:id', authMiddleware, QueueController.getQueues)
 routes.post('/queue', authMiddleware, validation(queueSchema), QueueController.createQueue)
 routes.put('/queue/:id', authMiddleware, validation(queueSchema), QueueController.updateQueue)
 routes.delete('/queue/:id', authMiddleware, QueueController.deleteQueue)
