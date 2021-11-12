b2c73a04f83246902283e41e6747ac6eb2092473
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-11 22:44:48 -0300

    Refactor: Using class instead of functions

--- src/controllers/QueueController.ts
+++ src/controllers/QueueController.ts
@@ -2,50 +2,54 @@ import { Request, Response } from 'express'
 import { Queue } from '../models/Queue'
 import { getRepository } from 'typeorm'
 
-export const getQueues = async (req: Request, res: Response) => {
-    const queue = await getRepository(Queue).find(
-        {
-            relations: ['userId']
+class QueueController{
+    async getQueues (req: Request, res: Response){
+        const queue = await getRepository(Queue).find(
+            {
+                relations: ['userId']
+            }
+        )
+    
+        if(!queue) {
+            return res.json({message: "Nenhuma fila cadastrada"})
         }
-    )
-
-    if(!queue) {
-        return res.json({message: "Nenhuma fila cadastrada"})
+    
+        return res.json(queue)
     }
 
-    return res.json(queue)
-}
-
-export const createQueue = async (req: Request, res: Response) => {
-    const newQueue = await getRepository(Queue).save(req.body)
-
-    return res.json({message: 'Cadastro feito com sucesso.'})
-}
-
-export const updateQueue = async (req: Request, res: Response) => {
-    const {id} = req.params
-    const {nome, timeout} = req.body
-
-    const queue = await getRepository(Queue).find({where: {id}})
-
-    if(!queue){
-        return res.status(404).json({message: "Não há nenhuma fila existente para atualizar."})
+    async createQueue (req: Request, res: Response) {
+        const newQueue = await getRepository(Queue).save(req.body)
+    
+        return res.json({message: 'Cadastro feito com sucesso.'})
     }
 
-    const updateQueue = await getRepository(Queue).query(`update queue set nome = '${nome}', timeout = '${timeout}' where id = ${id}`)
-
-    return res.status(200).json({message: "Fila atualizada com sucesso."})
-}
-
-export const deleteQueue = async (req: Request, res: Response) => {
-    const { id } = req.params
-
-    const queue = await getRepository(Queue).find({where: {id}})
-    if(!queue){
-        return res.status(404).json({message: "Não há fila para deletar."})
+    async updateQueue (req: Request, res: Response) {
+        const {id} = req.params
+        const {nome, timeout} = req.body
+    
+        const queue = await getRepository(Queue).find({where: {id}})
+    
+        if(!queue){
+            return res.status(404).json({message: "Não há nenhuma fila existente para atualizar."})
+        }
+    
+        const updateQueue = await getRepository(Queue).query(`update queue set nome = '${nome}', timeout = '${timeout}' where id = ${id}`)
+    
+        return res.status(200).json({message: "Fila atualizada com sucesso."})
     }
 
-    const deletedQueue = await getRepository(Queue).query(`delete from queue where id = ${id}`)
-
-    return res.status(200).json({message: "Fila deletada com sucesso."})
+    async deleteQueue (req: Request, res: Response) {
+        const { id } = req.params
+    
+        const queue = await getRepository(Queue).find({where: {id}})
+        if(!queue){
+            return res.status(404).json({message: "Não há fila para deletar."})
+        }
+    
+        const deletedQueue = await getRepository(Queue).query(`delete from queue where id = ${id}`)
+    
+        return res.status(200).json({message: "Fila deletada com sucesso."})
+    }
 }
+
+export default new QueueController()
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,65 +2,69 @@ import { Request, Response } from "express";
 import { getRepository } from 'typeorm'
 import { User } from '../models/User'
 
-export const getUsers = async (req: Request, res: Response) => {
-    const users = await getRepository(User).find()
+class UserController {
+    async getUsers (req: Request, res: Response) {
+        const users = await getRepository(User).find()
+        
+        if(users.length === 0){
+            return res.json({message: "Nenhum usuário cadastrado."})
+        }
     
-    if(users.length === 0){
-        return res.json({message: "Nenhum usuário cadastrado."})
+        return res.json(users)
     }
 
-    return res.json(users)
-}
-
-export const getOneUser = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const user = await getRepository(User).findOne({where: {id}})
-
-    if(!user){
-        return res.json({message: "Usuário não existente."})
-    }
+    async getOneUser (req: Request, res: Response) {
+        const { id } = req.params
+        const user = await getRepository(User).findOne({where: {id}})
     
-    return res.json(user)
-}
-
-export const createUser = async (req: Request, res: Response) => {
-    const { CPF } = req.body
-
-    const existUser = await getRepository(User).findOne({where: {CPF}})
-
-    if(existUser){
-        return res.status(400).json({message: "Usuário já existente"})
+        if(!user){
+            return res.json({message: "Usuário não existente."})
+        }
+        
+        return res.json(user)
     }
 
-    const user = await getRepository(User).save(req.body)
-
-    return res.json(user)
-}
-
-export const updateUser = async (req: Request, res: Response) => {
-    const { id } = req.params
+    async createUser (req: Request, res: Response) {
+        const { CPF } = req.body
     
-    const user = await getRepository(User).findOne({where: {id}})
-
-    if(!user){
-        return res.status(404).json({message: 'Usuário não existente.'})
+        const existUser = await getRepository(User).findOne({where: {CPF}})
+    
+        if(existUser){
+            return res.status(400).json({message: "Usuário já existente"})
+        }
+    
+        const user = await getRepository(User).save(req.body)
+    
+        return res.json(user)
     }
 
-    const updatedUser = await getRepository(User).save(req.body)
-
-    return res.status(200).json({message:'Usuário atualizado com sucesso.'})
-
-}
-
-export const deleteUser = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const user = await getRepository(User).findOne({where: {id}})
-
-    if(!user){
-        return res.status(404).json({message: 'Usuário não existente.'})
+    async updateUser (req: Request, res: Response) {
+        const { id } = req.params
+        
+        const user = await getRepository(User).findOne({where: {id}})
+    
+        if(!user){
+            return res.status(404).json({message: 'Usuário não existente.'})
+        }
+    
+        const updatedUser = await getRepository(User).save(req.body)
+    
+        return res.status(200).json({message:'Usuário atualizado com sucesso.'})
+    
     }
 
-    const deleted = await getRepository(User).query(`delete from users where id = ${id}`)
-
-    return res.status(200).json({message: 'Usuário deletado com sucesso.'})
+    async deleteUser (req: Request, res: Response) {
+        const { id } = req.params
+        const user = await getRepository(User).findOne({where: {id}})
+    
+        if(!user){
+            return res.status(404).json({message: 'Usuário não existente.'})
+        }
+    
+        const deleted = await getRepository(User).query(`delete from users where id = ${id}`)
+    
+        return res.status(200).json({message: 'Usuário deletado com sucesso.'})
+    }
 }
+
+export default new UserController()
