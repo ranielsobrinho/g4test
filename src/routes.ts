46fc9110cc980a53f1cbaa9804cdaf37c73ed712
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-11 22:47:55 -0300

    Refact using classes and add  auth route

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,31 @@
 import { Router, Request, Response } from 'express'
 
-import { createUser, deleteUser, getOneUser, getUsers, updateUser } from './controllers/UserController'
-import { createQueue, deleteQueue, getQueues, updateQueue } from './controllers/QueueController'
+import UserController from './controllers/UserController'
+import QueueController from './controllers/QueueController'
+import AuthController from './controllers/AuthController'
+import authMiddleware from './middlewares/authMiddleware'
+
 import validation from './middlewares/validationMiddleware'
 import userSchema from './validations/userValidation'
 import queueSchema from './validations/queueValidation'
+import authSchema from './validations/authValidation'
 
 const routes = Router();
 
 routes.get('/', (req: Request, res: Response) => {
     return res.json({message: "Bem-vindo ao CRUD de usuário e fila.!"})
 })
-routes.get('/users', getUsers)
-routes.get('/users/:id', getOneUser)
-routes.post('/users', validation(userSchema),createUser)
-routes.put('/users/:id', validation(userSchema), updateUser)
-routes.delete('/users/:id', deleteUser)
+routes.get('/users', UserController.getUsers)
+routes.get('/users/:id', UserController.getOneUser)
+routes.post('/users', validation(userSchema), UserController.createUser)
+routes.put('/users/:id', validation(userSchema), UserController.updateUser)
+routes.delete('/users/:id', UserController.deleteUser)
 
-routes.get('/queue', getQueues)
-routes.post('/queue', validation(queueSchema),createQueue)
-routes.put('/queue/:id', validation(queueSchema), updateQueue)
-routes.delete('/queue/:id', deleteQueue)
+routes.get('/queue', authMiddleware, QueueController.getQueues)
+routes.post('/queue', authMiddleware, validation(queueSchema), QueueController.createQueue)
+routes.put('/queue/:id', authMiddleware, validation(queueSchema), QueueController.updateQueue)
+routes.delete('/queue/:id', authMiddleware, QueueController.deleteQueue)
+
+routes.post('/auth', validation(authSchema),AuthController.authenticate)
 
 export default routes;
